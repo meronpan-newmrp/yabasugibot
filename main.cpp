f1c81ccebed0908dbe9c853fc64132011b88edde
meronpan-newmrp <chacoco.4989@icloud.com>
2026-06-02 20:13:57 +0900

    update bot

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <cstdlib>
 
 int main() {
-    // 環境変数からトークン取得（安全）
+    const dpp::snowflake MRS_ID = 1298304487605403658;
+
+    // トークンの取得
     const char* token = std::getenv("DISCORD_TOKEN");
     if (!token) {
         std::cout << "DISCORD_TOKEN not set!\n";
@@ -24,25 +26,49 @@ int main() {
                 dpp::presence(
                     dpp::ps_online,
                     dpp::at_game,
-                    "WSL / GCE Bot"
+                    "やばー"
                 )
             );
         }
     });
 
+    bot.guild_command_create(
+        dpp::slashcommand("sendmsg", "メッセージを送信", bot.me.id),
+        .add_option(
+            dpp::command_option(dpp::co_channel, "channel", "送信するチャンネル", true)
+        )
+        .add_option(
+            dpp::command_option(dpp::co_string, "message", "送信するメッセージ", true)
+        ),
+        MRS_ID
+    );
+
+    //コマンド
+    bot.on_slashcommand([&bot](const dpp::slashcommand_t& event) {
+        if (event.command.get_command_name() == "sendmsg") {
+            auto channel_id = std::get<dpp::snowflake>(event.get_parameter("channel"));
+            auto message = std::get<std::string>(event.get_parameter("message"));
+
+            bot.message_create(
+                dpp::message(channel_id, message)
+            );
+
+            event.reply("メッセージを送信完了！", true);
+        }
+    });
     // メッセージ受信イベント
     bot.on_message_create([&bot](const dpp::message_create_t& event) {
         auto msg = event.msg;
 
         if (msg.content == "!ping") {
             bot.message_create(
-                dpp::message(msg.channel_id, "pong 🏓")
+                dpp::message(msg.channel_id, "ピンポォン！！")
             );
         }
 
         if (msg.content == "!hello") {
             bot.message_create(
-                dpp::message(msg.channel_id, "こんにちは！botだよ 🤖")
+                dpp::message(msg.channel_id, "やあ！ぼくはパスタ。")
             );
         }
     });
