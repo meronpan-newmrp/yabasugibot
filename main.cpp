b5ddc31c99abc74720e802b55fa034624d9444e0
meronpan-newmrp <chacoco.4989@icloud.com>
2026-06-02 21:20:42 +0900

    update bot

--- main.cpp
+++ main.cpp
@@ -63,7 +63,8 @@ int main() {
 
         if (event.command.get_command_name() == "sendmsg") {
 
-            auto channel_id = std::stoull(std::get<std::string>(event.get_parameter("channel")));
+            auto channel_id = std::stoull(std::get<std::string>(event.get_parameter("channelgit add main.cpp
+)));
 
             auto message = std::get<std::string>(
                 event.get_parameter("message")
